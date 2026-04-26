ba0c67ef8394d3df4e1f2547f6e4895e4887df82
FeatherHub <planomene@naver.com>
2026-04-26 16:21:34 +0900

    add Projectile class & BP. add custom collision profile "Projectile".

---
+++ Source/ActionRoguelike/Projectile/RogueProjectileMagic.cpp
@@ -0,0 +1,15 @@
+﻿#include "RogueProjectileMagic.h"
+
+#include "Components/SphereComponent.h"
+
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComponent->SetCollisionProfileName("Projectile");
+	RootComponent = SphereComponent;
+
+	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovementComp"));
+	MovementComponent->InitialSpeed = 1000.f;
+}
---
+++ Source/ActionRoguelike/Projectile/RogueProjectileMagic.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileMagic.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueProjectileMagic : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category=Component)
+	TObjectPtr<USphereComponent> SphereComponent;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Component)
+	TObjectPtr<UProjectileMovementComponent> MovementComponent;
+	
+public:
+	ARogueProjectileMagic();
+
+};
