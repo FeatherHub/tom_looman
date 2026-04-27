bbb2a63284ab079b4bdc574f283c45400c161ce8
FeatherHub <planomene@naver.com>
2026-04-27 20:29:32 +0900

    add ExplosiveBarrel
    1. enable physics & collision on BoxShapeComp
    2. adjust box shape lengths
    3. assign mesh

---
+++ Source/ActionRoguelike/World/RogueExplosiveBarrel.cpp
@@ -0,0 +1,15 @@
+﻿#include "RogueExplosiveBarrel.h"
+
+#include "Components/BoxComponent.h"
+
+ARogueExplosiveBarrel::ARogueExplosiveBarrel()
+{
+	SMComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComp"));
+	BoxShapeComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxShapeComp"));
+
+	RootComponent = BoxShapeComp; 
+	SMComp->SetupAttachment(BoxShapeComp);
+
+	BoxShapeComp->SetSimulatePhysics(true);
+	BoxShapeComp->SetEnableGravity(true);
+}
---
+++ Source/ActionRoguelike/World/RogueExplosiveBarrel.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueExplosiveBarrel.generated.h"
+
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UStaticMeshComponent> SMComp;
+	
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UBoxComponent> BoxShapeComp;
+	
+public:
+	ARogueExplosiveBarrel();
+};
