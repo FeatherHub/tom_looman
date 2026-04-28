16afee106aea2c12762ac33cea0093382f091f99
FeatherHub <planomene@naver.com>
2026-04-28 18:33:30 +0900

    Add ItemChest Actor
    1. add ItemChest's Base & Lid StaticMeshComponent
    2. add ItemChest Lid opening animation

---
+++ Source/ActionRoguelike/World/RogueItemChest.cpp
@@ -0,0 +1,36 @@
+﻿#include "RogueItemChest.h"
+
+
+ARogueItemChest::ARogueItemChest()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	BaseMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMeshComp"));
+	RootComponent = BaseMeshComp;
+
+	LidMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("LidMeshComp"));
+	LidMeshComp->SetupAttachment(BaseMeshComp);
+}
+
+void ARogueItemChest::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PrimaryActorTick.SetTickFunctionEnable(true);
+}
+
+void ARogueItemChest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FRotator NewRotator = FMath::RInterpConstantTo(FRotator{AnimationPitchCurrent, 0.f, 0.f}, FRotator{AnimationPitchMax, 0.f, 0.f}, DeltaTime, AnimationPitchSpeed);
+	AnimationPitchCurrent = NewRotator.Pitch;
+
+	LidMeshComp->SetRelativeRotation(NewRotator);
+	
+	if (FMath::IsNearlyEqual(AnimationPitchCurrent, AnimationPitchMax))
+	{
+		PrimaryActorTick.SetTickFunctionEnable(false);
+	}
+}
---
+++ Source/ActionRoguelike/World/RogueItemChest.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueItemChest.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueItemChest : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UStaticMeshComponent> LidMeshComp;
+	
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UStaticMeshComponent> BaseMeshComp;
+
+	UPROPERTY(EditDefaultsOnly, Category=Animation)
+	float AnimationPitchMax = 120.f;
+
+	UPROPERTY(EditDefaultsOnly, Category=Animation)
+	float AnimationPitchSpeed = 20.f;
+	
+	float AnimationPitchCurrent = 0.f;
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	ARogueItemChest();
+	virtual void Tick(float DeltaTime) override;
+};
