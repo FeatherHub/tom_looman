45392d24ad73c18f2680c2264130b7a02abba999
FeatherHub <planomene@naver.com>
2026-04-29 15:41:54 +0900

    Add Blackhole Projectile

---
+++ Source/ActionRoguelike/Projectile/RogueProjectileBlackhole.cpp
@@ -0,0 +1,58 @@
+﻿#include "RogueProjectileBlackhole.h"
+
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+
+ARogueProjectileBlackhole::ARogueProjectileBlackhole()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	ProjectileMovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComp"));
+	InFlightNiagaraComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("InFlightNiagaraComp"));
+	InFlightAudioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("InFlightAudioComp"));
+	BlackholeRadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("BlackholdRadialForceComp"));
+	
+	RootComponent = SphereComp;
+	InFlightAudioComp->SetupAttachment(SphereComp);
+	InFlightNiagaraComp->SetupAttachment(SphereComp);
+	BlackholeRadialForceComp->SetupAttachment(SphereComp);
+	
+	ProjectileMovementComp->ProjectileGravityScale = 0.f;
+	ProjectileMovementComp->InitialSpeed = 300.f;
+	
+	BlackholeRadialForceComp->bAutoActivate = true;
+	BlackholeRadialForceComp->ForceStrength = -10000000;
+}
+
+void ARogueProjectileBlackhole::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBeginOverlap);
+}
+
+void ARogueProjectileBlackhole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ThisClass::OnExplode, 5.f);
+}
+
+void ARogueProjectileBlackhole::OnExplode()
+{
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, ExplosionNiagaraEffect, GetActorLocation());
+	UGameplayStatics::PlaySoundAtLocation(this, ExplosionSoundEffect, GetActorLocation(), FRotator::ZeroRotator);
+	
+	Destroy();
+}
+
+void ARogueProjectileBlackhole::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult )
+{
+	Other->Destroy();
+}
---
+++ Source/ActionRoguelike/Projectile/RogueProjectileBlackhole.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileBlackhole.generated.h"
+
+class URadialForceComponent;
+class UNiagaraSystem;
+class UNiagaraComponent;
+class UProjectileMovementComponent;
+class USphereComponent;
+class USoundBase;
+class UAudioComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueProjectileBlackhole : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<USphereComponent> SphereComp;
+	
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComp;
+	
+	UPROPERTY(VisibleAnywhere, Category="Component|Effect")
+	TObjectPtr<UNiagaraComponent> InFlightNiagaraComp;
+
+	UPROPERTY(VisibleAnywhere, Category="Component|Effect")
+	TObjectPtr<UAudioComponent> InFlightAudioComp;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TObjectPtr<UNiagaraSystem> ExplosionNiagaraEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TObjectPtr<USoundBase> ExplosionSoundEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category="Effect|Attack")
+	TObjectPtr<URadialForceComponent> BlackholeRadialForceComp;
+	
+protected:
+	void OnExplode();
+	
+public:
+	ARogueProjectileBlackhole();
+	virtual void PostInitializeComponents() override;
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult );
+};
