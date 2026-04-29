aebc1308971632f90a6373e7a0a09d3ec18c87e5
FeatherHub <planomene@naver.com>
2026-04-29 18:37:37 +0900

    Add Teleport Projectile

---
+++ Source/ActionRoguelike/Projectile/RogueProjectileTeleport.cpp
@@ -0,0 +1,65 @@
+﻿#include "RogueProjectileTeleport.h"
+
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ARogueProjectileTeleport::ARogueProjectileTeleport()
+{
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	InFlightNiagaraComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("InFlightNiagaraComp"));
+	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovementComp"));
+	
+	RootComponent = SphereComp;
+	InFlightNiagaraComp->SetupAttachment(SphereComp);
+
+	SphereComp->SetCollisionProfileName("Projectile");
+	
+	InFlightNiagaraComp->bAutoActivate = true;
+	
+	MovementComp->InitialSpeed = 500.f;
+	MovementComp->ProjectileGravityScale = 0.f;
+	
+	TeleportDelay = 3.f;
+}
+
+void ARogueProjectileTeleport::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+	
+	SphereComp->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+}
+
+void ARogueProjectileTeleport::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GetWorldTimerManager().SetTimer(TeleportTimerHandle, this, &ThisClass::Teleport, TeleportDelay);
+
+	UGameplayStatics::PlaySoundAtLocation(this, SpawnSoundEffect, GetActorLocation(), FRotator::ZeroRotator);
+}
+
+
+void ARogueProjectileTeleport::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	Teleport();
+}
+
+void ARogueProjectileTeleport::Teleport()
+{
+	GetWorldTimerManager().ClearTimer(TeleportTimerHandle);
+
+	APawn* Shooter = GetInstigator();
+	if (IsValid(Shooter))
+	{
+		Shooter->SetActorLocation(GetActorLocation());
+	}
+
+	UGameplayStatics::PlaySoundAtLocation(this, TeleportSoundEffect, GetActorLocation(), FRotator::ZeroRotator);
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, TeleportNiagaraEffect, GetActorLocation());
+	
+	Destroy();
+}
---
+++ Source/ActionRoguelike/Projectile/RogueProjectileTeleport.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileTeleport.generated.h"
+
+class UProjectileMovementComponent;
+class USphereComponent;
+class UNiagaraSystem;
+class UNiagaraComponent;
+class UAudioComponent;
+class USoundBase;
+
+UCLASS()
+class ACTIONROGUELIKE_API ARogueProjectileTeleport : public AActor
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	TObjectPtr<USphereComponent> SphereComp;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+	TObjectPtr<UProjectileMovementComponent> MovementComp;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Component|Effect")
+	TObjectPtr<UNiagaraComponent> InFlightNiagaraComp;
+	
+	UPROPERTY(EditDefaultsOnly, Category = Effect)
+	TObjectPtr<USoundBase> SpawnSoundEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Effect|Teleport")
+	TObjectPtr<UNiagaraSystem> TeleportNiagaraEffect;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Effect|Teleport")
+	TObjectPtr<USoundBase> TeleportSoundEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category = Teleport)
+	float TeleportDelay;
+	
+	FTimerHandle TeleportTimerHandle;
+	
+protected:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void Teleport();
+	
+public:
+	ARogueProjectileTeleport();
+	virtual void PostInitializeComponents() override;
+	virtual void BeginPlay() override;
+};
