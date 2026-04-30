c9d47afc6b14d41d0ecd1fa5238f52c6459be7f1
FeatherHub <planomene@naver.com>
2026-04-30 15:06:38 +0900

    Refactor: factor out Projectile Base from 3 projectiles

---
+++ Source/ActionRoguelike/Projectile/RogueProjectileBase.cpp
@@ -0,0 +1,39 @@
+﻿#include "RogueProjectileBase.h"
+
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "Components/AudioComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+ARogueProjectileBase::ARogueProjectileBase()
+{
+	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovementComp"));
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	InFlightNiagaraComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("InFlightNiagaraComp"));
+	InFlightAudioComp = CreateDefaultSubobject<UAudioComponent>(TEXT("InFlightAudioComp"));
+	
+	RootComponent = SphereComp;
+
+	InFlightAudioComp->SetupAttachment(SphereComp);
+	InFlightNiagaraComp->SetupAttachment(SphereComp);
+	
+	MovementComp->ProjectileGravityScale = 0.f;
+}
+
+void ARogueProjectileBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	SphereComp->IgnoreActorWhenMoving(GetInstigator(), true);
+}
+
+void ARogueProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UGameplayStatics::PlaySoundAtLocation(this, SpawnSoundEffect, GetActorLocation(), GetActorRotation());
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, SpawnNiagaraEffect, GetActorLocation(), GetActorRotation());
+}
---
+++ Source/ActionRoguelike/Projectile/RogueProjectileBase.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RogueProjectileBase.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UNiagaraSystem;
+class UNiagaraComponent;
+
+UCLASS(Abstract)
+class ACTIONROGUELIKE_API ARogueProjectileBase : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UProjectileMovementComponent> MovementComp;
+
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<USphereComponent> SphereComp;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TObjectPtr<UNiagaraSystem> SpawnNiagaraEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TObjectPtr<USoundBase> SpawnSoundEffect;
+	
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UNiagaraComponent> InFlightNiagaraComp;
+
+	UPROPERTY(VisibleAnywhere, Category=Component)
+	TObjectPtr<UAudioComponent> InFlightAudioComp;
+	
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TObjectPtr<UNiagaraSystem> ExplosionNiagaraEffect;
+
+	UPROPERTY(EditDefaultsOnly, Category=Effect)
+	TObjectPtr<USoundBase> ExplosionSoundEffect;
+
+protected:
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) {};
+	
+	UFUNCTION()
+	virtual void OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult ) {};
+	
+public:
+	ARogueProjectileBase();
+	virtual void PostInitializeComponents() override;
+
+protected:
+	virtual void BeginPlay() override;
+};
