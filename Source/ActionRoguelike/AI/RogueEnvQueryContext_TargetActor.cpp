0bb8d984be68bfd3ab90f69a36e2c9ea8f44743f
FeatherHub <planomene@naver.com>
2026-05-02 21:17:38 +0900

    Add EQS Query for MoveTo task

---
+++ Source/ActionRoguelike/AI/RogueEnvQueryContext_TargetActor.cpp
@@ -0,0 +1,21 @@
+﻿#include "RogueEnvQueryContext_TargetActor.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/Items/EnvQueryItemType_Actor.h"
+
+void URogueEnvQueryContext_TargetActor::ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const
+{
+	APawn* AIPawn = Cast<APawn>(QueryInstance.Owner.Get());
+	AAIController* AIController = Cast<AAIController>(AIPawn->GetController());
+
+	UBlackboardComponent* BBComp = AIController->GetBlackboardComponent();
+
+	FName TargetActorName = TEXT("PlayerActor");
+	AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject(TargetActorName));
+	if (TargetActor)
+	{
+		UEnvQueryItemType_Actor::SetContextHelper(ContextData, TargetActor);
+	}
+}
---
+++ Source/ActionRoguelike/AI/RogueEnvQueryContext_TargetActor.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "RogueEnvQueryContext_TargetActor.generated.h"
+
+
+UCLASS()
+class ACTIONROGUELIKE_API URogueEnvQueryContext_TargetActor : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void ProvideContext(FEnvQueryInstance& QueryInstance, FEnvQueryContextData& ContextData) const override;
+};
