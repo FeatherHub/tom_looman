6dc508fea3e56392fd10c6ca66bfe387436c0c82
FeatherHub <planomene@naver.com>
2026-05-02 17:59:26 +0900

    Add BTTask RangeAttack

---
+++ Source/ActionRoguelike/AI/RogueBTTask_RangedAttack.cpp
@@ -0,0 +1,33 @@
+﻿#include "RogueBTTask_RangedAttack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Character.h"
+#include "Projectile/RogueProjectileBase.h"
+
+EBTNodeResult::Type URogueBTTask_RangedAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+
+	AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (!IsValid(TargetActor))
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	ACharacter* AICharacter = Cast<ACharacter>(AIController->GetPawn());
+	FVector SpawnLocation = AICharacter->GetMesh()->GetSocketLocation(MuzzleSocketName);
+
+	FRotator SpawnRotation = (TargetActor->GetActorLocation() - SpawnLocation).Rotation();
+	SpawnRotation.Pitch += FMath::RandRange(0.f, BulletSpreadDegree);
+	SpawnRotation.Yaw += FMath::RandRange(-BulletSpreadDegree, BulletSpreadDegree);
+	
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Instigator = AICharacter;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	AActor* NewProjectile = GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnLocation, SpawnRotation, SpawnParams);
+
+	return NewProjectile ? EBTNodeResult::Succeeded : EBTNodeResult::Failed;
+}
---
+++ Source/ActionRoguelike/AI/RogueBTTask_RangedAttack.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "RogueBTTask_RangedAttack.generated.h"
+
+class ARogueProjectileBase;
+
+UCLASS()
+class ACTIONROGUELIKE_API URogueBTTask_RangedAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+protected:
+	UPROPERTY(EditAnywhere, Category=Attack)
+	FName MuzzleSocketName;
+	 
+	UPROPERTY(EditAnywhere, Category=Attack)
+	FBlackboardKeySelector TargetActorKey;
+	
+	UPROPERTY(EditAnywhere, Category=Attack)
+	TSubclassOf<ARogueProjectileBase> ProjectileClass;
+
+	UPROPERTY(EditAnywhere, Category=Attack)
+	float BulletSpreadDegree = 10.f;
+
+public:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
