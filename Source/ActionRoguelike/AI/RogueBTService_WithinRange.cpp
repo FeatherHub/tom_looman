db160a607fc981e8903c1aa1bbbd52aca491dbc8
FeatherHub <planomene@naver.com>
2026-05-02 16:59:55 +0900

    Add BTService WithinRange

---
+++ Source/ActionRoguelike/AI/RogueBTService_WithinRange.cpp
@@ -0,0 +1,26 @@
+﻿#include "RogueBTService_WithinRange.h"
+
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void URogueBTService_WithinRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+	
+	UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	
+	AActor* TargetActor = Cast<AActor>(BBComp->GetValueAsObject(TargetActorKey.SelectedKeyName));
+	if (TargetActor)
+	{
+		AActor* AIActor = AIController->GetPawn();
+		check(AIActor)
+
+		float DistanceTo = FVector::Distance(TargetActor->GetActorLocation(), AIActor->GetActorLocation());
+		
+		bool bIsWithinRange = DistanceTo < MaxRange;
+		bool bHasLOS = AIController->LineOfSightTo(TargetActor);
+
+		BBComp->SetValueAsBool(WithinRangeKey.SelectedKeyName, bIsWithinRange && bHasLOS);
+	}
+}
---
+++ Source/ActionRoguelike/AI/RogueBTService_WithinRange.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "RogueBTService_WithinRange.generated.h"
+
+UCLASS()
+class ACTIONROGUELIKE_API URogueBTService_WithinRange : public UBTService
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere)
+	FBlackboardKeySelector TargetActorKey;
+
+	UPROPERTY(EditAnywhere)
+	FBlackboardKeySelector WithinRangeKey;
+
+	UPROPERTY(EditAnywhere)
+	float MaxRange = 500.f;
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
